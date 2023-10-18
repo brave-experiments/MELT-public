b30a85b02e99519aaac7757c4f0b0b3cc11399c9
Stefanos Laskaridis <stefanos@brave.com>
2023-10-18 15:41:59 +0100

    Add llama.cpp support for parsers

--- src/parsers/README.md
+++ src/parsers/README.md
@@ -4,9 +4,14 @@ This is a parser for the JSONs output when profiling with TVM.
 
 ## How to run
 
+### For local logs
+
 ```
-python parser.py --mode file -i tests/dummy_file.log [-o CSVs] [--merge <per_op|per_module>]
+python parser.py --mode file -i test_files/dummy_file.mlc.log [-o CSVs] [--merge <per_op|per_module>] -b mlc
+python parser.py --mode file -i test_files/dummy_file.llama_cpp.log [-o CSVs] [--merge <per_op|per_module>] -b llama.cpp
 ```
+
+### For Android residing logs
 
 ```
 [ADB_RUNNER=<path_to_adb> DEVICE_ID=<android_device_id> LOGFILE_ROOT=<path_to_save_locally_on_device>] ./logcat_getter.sh
--- src/parsers/parser.py
+++ src/parsers/parser.py
@@ -2,81 +2,90 @@
 import os
 import pandas as pd
 
-from utils import (
+from utils.utils import (
     parse_args,
     parse_file,
-    parse_config,
-    normalize_calls,
     merge_ops
 )
 
+
 def main(args):
     if args.mode == 'file':
-        ops_metrics = parse_file(args.input)
-        ops_count = {op: 0 for op in list(set([op for op, _ in ops_metrics]))}
-        calls_dfs = {}
-        device_metrics_dfs = {}
-        config_dfs = {}
-        for op, metrics_dict in ops_metrics:
-            calls_df = pd.DataFrame(metrics_dict['calls'])
-            device_metrics_df = pd.DataFrame(metrics_dict['device_metrics'])
-            config_df = pd.DataFrame(parse_config(metrics_dict['configuration']))
-
-            calls_df = normalize_calls(calls_df)
-            device_metrics_df = normalize_calls(device_metrics_df.transpose())
-
-            calls_dfs[f"{op}_{ops_count[op]}"] = calls_df
-            device_metrics_dfs[f"{op}_{ops_count[op]}"] = device_metrics_df
-            config_dfs[f"{op}_{ops_count[op]}"] = config_df
-            ops_count[op] += 1
-
-            if args.verbose:
-                print("==="*3,  op, "==="*3)
-                print(device_metrics_df)
-                print(config_df)
-                print(calls_df)
+        if args.backend == 'mlc':
+            calls_dfs, device_metrics_dfs, config_dfs = parse_file(args.input,
+                                                                   args.backend,
+                                                                   args.verbose)
+        elif args.backend == 'llama.cpp':
+            weights_df, calls_summary_dfs, calls_dfs = parse_file(args.input, args.backend, args.verbose)
 
     elif args.mode == 'logcat':
         raise NotImplementedError
     elif args.mode == 'ios':
         raise NotImplementedError
     else:
-        ValueError('Invalid mode: {}'.format(args.mode))
+        raise ValueError('Invalid mode: {}'.format(args.mode))
 
     if args.merge:
-        count_per_module = {}
-        for k, df in calls_dfs.items():
-            op = k.split('_')[0]
-            df['Module'] = op
-            count_per_module[op] = count_per_module.get(op, 0) + 1
-        df_merged = pd.concat([*calls_dfs.values()])
+        if args.backend == 'mlc':
+            count_per_module = {}
+            for k, df in calls_dfs.items():
+                op = k.split('_')[0]
+                df['Module'] = op
+                count_per_module[op] = count_per_module.get(op, 0) + 1
+            df_merged = pd.concat([*calls_dfs.values()])
 
-        if args.merge == 'per_module':
-            print(f"Merging per module: {set([k.split('_')[0] for k in calls_dfs.keys()])}")
-            df_merged = merge_ops(df_merged, group_cols=['Module'], drop_cols=['Device', 'Name'])
-            for module in df_merged.index:
-                df_merged.loc[module, 'Count'] = count_per_module[module]
+            if args.merge == 'per_module':
+                print(f"Merging per module: {set([k.split('_')[0] for k in calls_dfs.keys()])}")
+                df_merged = merge_ops(df_merged, group_cols=['Module'], drop_cols=['Device', 'Name', 'Percent', 'Argument Shapes',])
+                for module in df_merged.index:
+                    df_merged.loc[module, 'Count'] = count_per_module[module]
 
-        elif args.merge == 'per_op':
-            print(f"Merging across modules: {set([k.split('_')[0] for k in calls_dfs.keys()])}")
-            df_merged = merge_ops(df_merged, ['Name', 'Device'], drop_cols=['Module'])
+            elif args.merge == 'per_op':
+                print(f"Merging across modules: {set([k.split('_')[0] for k in calls_dfs.keys()])}")
+                df_merged = merge_ops(df_merged, ['Name', 'Device'], drop_cols=['Module', 'Percent', 'Argument Shapes',])
 
-        else:
-            ValueError(f"Invalid merge mode: {args.merge}")
+            else:
+                raise ValueError(f"Invalid merge mode: {args.merge}")
 
-        if args.verbose:
-            print(df_merged)
+            if args.verbose:
+                print(df_merged)
+
+        elif args.backend == 'llama.cpp':
+            if args.merge == 'per_module':
+                df_merged_detailed = merge_ops(calls_dfs, ['node_num', 'Name'],
+                                      drop_cols=["Argument Shapes"])
+            elif args.merge == 'per_op':
+                df_merged_detailed = merge_ops(calls_dfs, ['Name'],
+                                               drop_cols=["Argument Shapes", "node_num"])
+                df_merged_summary = merge_ops(calls_summary_dfs, ['Name'])
+
 
     if args.output:
         os.makedirs(args.output, exist_ok=True)
 
-        for key in calls_dfs.keys():
-            calls_dfs[key].to_csv(os.path.join(args.output, f'calls_{key}.csv'), index=False)
-            device_metrics_dfs[key].to_csv(os.path.join(args.output, f'device_metrics_{key}.csv'), index=False)
-            config_dfs[key].to_csv(os.path.join(args.output, f'config_{key}.csv'), index=False)
+        if args.backend == 'mlc':
+            for key, _ in calls_dfs.items():
+                calls_dfs[key].to_csv(os.path.join(args.output, f'calls_{key}.csv'), index=False)
+                device_metrics_dfs[key].to_csv(os.path.join(args.output, f'device_metrics_{key}.csv'),
+                                               index=False)
+                config_dfs[key].to_csv(os.path.join(args.output, f'config_{key}.csv'), index=False)
 
-        if args.merge:
-            df_merged.to_csv(os.path.join(args.output, f'calls_merged_{args.merge}.csv'), index=True)
+            if args.merge:
+                df_merged.to_csv(os.path.join(args.output, f'calls_merged_{args.merge}.csv'),
+                                index=True)
+        elif args.backend == 'llama.cpp':
+            weights_df.to_csv(os.path.join(args.output, 'weights.csv'), index=False)
+            calls_summary_dfs.to_csv(os.path.join(args.output, 'calls_summary.csv'), index=False)
+            calls_dfs.to_csv(os.path.join(args.output, 'calls.csv'), index=False)
+
+            if args.merge:
+                df_merged_detailed.to_csv(
+                    os.path.join(args.output, f'calls_merged_detailed_{args.merge}.csv'),
+                    index=True)
+                if args.merge == 'per_op':
+                    df_merged_summary.to_csv(
+                        os.path.join(args.output, f'calls_merged_summary_{args.merge}.csv'),
+                        index=True)
 
 
 if __name__ == '__main__':
