36fcf0289858c3bc22d95e7ecbf7954e377a73bb
Stefanos Laskaridis <stefanos@brave.com>
2024-03-11 20:18:20 +0000

    More configurable logcat getter script

--- src/parsers/scripts/logcat_getter.sh
+++ src/parsers/scripts/logcat_getter.sh
@@ -7,10 +7,15 @@ NOW=$(date +"%Y%m%d%H%M%S")
 
 # This script is used to get logcat from android device
 echo "Running logcat on device, persisting to $LOGFILE_ROOT/logcat_$NOW.txt"
-$ADB_RUNNER -s $DEVICE_ID logcat -f $LOGFILE_ROOT/logcat_$NOW.txt -d -v raw -e "Report" "TVM_RUNTIME:I" "*:S"
+$ADB_RUNNER -s $DEVICE_ID logcat -f $LOGFILE_ROOT/logcat_$NOW.txt -d -v raw "TVM_RUNTIME:I" "*:S"
 
 echo "Getting log file from device"
 $ADB_RUNNER -s $DEVICE_ID pull $LOGFILE_ROOT/logcat_$NOW.txt ./logcat_$NOW.txt
+
+echo "Filtering lines"
+grep "Report" -A1 logcat_$NOW.txt > logcat_$NOW.txt_
+rm logcat_$NOW.txt
+mv logcat_$NOW.txt_ logcat_$NOW.txt
 
 echo "Removing log file from device"
 $ADB_RUNNER -s $DEVICE_ID shell rm $LOGFILE_ROOT/logcat_$NOW.txt
