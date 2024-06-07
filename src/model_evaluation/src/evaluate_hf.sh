00a3f4a9b9f2842991f85395701671a2ea8b74ec
Stefanos Laskaridis <stefanos@brave.com>
2024-06-07 18:15:39 +0100

    Clean up evaluation scripts

--- src/model_evaluation/src/evaluate_hf.sh
+++ src/model_evaluation/src/evaluate_hf.sh
@@ -1,6 +1,17 @@
 #!/bin/bash
 
-MODELS=(google_gemma-2b google_gemma-7b)
+# Note:   Script to evaluate the performance of the HF models with lm-eval suite.
+# Author: Stefanos Laskaridis
+
+MODELS=(
+    TinyLlama_TinyLlama-1.1B-Chat-v0.5
+    stabilityai_stablelm-zephyr-3b
+    mistralai_Mistral-7B-Instruct-v0.1
+    meta-llama_Llama-2-7b-hf
+    meta-llama_Llama-2-13b-hf
+    google_gemma-2b
+    google_gemma-7b
+)
 TASKS=(winogrande arc_easy arc_challenge truthfulqa hellaswag)
 
 
